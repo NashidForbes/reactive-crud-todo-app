d7d24899eeb5ed5e9adc8f3fefffa21753389a25
NashidForbes <NashidForbes@biohumans.com>
2021-10-05 01:10:32 -0400

    Updated api annotation to use proper v2 path notation.

--- src/main/java/com/quicktutorialz/nio/endpoints/v2/ToDoEndpoints.java
+++ src/main/java/com/quicktutorialz/nio/endpoints/v2/ToDoEndpoints.java
@@ -18,7 +18,7 @@ public class ToDoEndpoints extends Endpoints {
     ToDoDao toDoDao = ToDoDaoImpl.getInstance();
 
     // @Api annotation is just a description of the action method, it's not needed.
-    @Api(path = "/api/v1/create", method = "POST", consumes = "application/json", produces = "application/json"
+    @Api(path = "/api/v2/create", method = "POST", consumes = "application/json", produces = "application/json"
             , description = "")
     Action createToDo = (HttpServletRequest req, HttpServletResponse resp) -> {
         Observable.just(req)
@@ -30,7 +30,7 @@ public class ToDoEndpoints extends Endpoints {
 //        toJsonResponse(req, resp, new ResponseDto(200, outPut));
     };
 
-    @Api(path = "/api/v1/read/{id}", method = "GET", produces = "application/json", description = "")
+    @Api(path = "/api/v2/read/{id}", method = "GET", produces = "application/json", description = "")
     Action readToDo = (HttpServletRequest req, HttpServletResponse resp) -> {
         Observable.just(req)
                 .map(request -> getPathVariables(req).get("id"))
@@ -44,7 +44,7 @@ public class ToDoEndpoints extends Endpoints {
 //                outPut.get() : "todo not found"));
     };
 
-    @Api(path = "/api/v1/read", method = "GET", produces = "application/json", description = "")
+    @Api(path = "/api/v2/read", method = "GET", produces = "application/json", description = "")
     Action readAllToDos = (HttpServletRequest req, HttpServletResponse resp) -> {
         Observable.just(req)
                 .flatMap(request -> toDoDao.readAll())
@@ -55,7 +55,7 @@ public class ToDoEndpoints extends Endpoints {
 //        toJsonResponse(req, resp, new ResponseDto(200, outPutAll));
     };
 
-    @Api(path = "/api/v1/update", method = "POST", consumes = "application/json", produces = "application/json",
+    @Api(path = "/api/v2/update", method = "POST", consumes = "application/json", produces = "application/json",
             description = "")
     Action updateToDo = (HttpServletRequest req, HttpServletResponse resp) -> {
         Observable.just(req)
@@ -70,7 +70,7 @@ public class ToDoEndpoints extends Endpoints {
 //        toJsonResponse(req, resp, new ResponseDto(200, outPut.isPresent() ? outPut.get() : "to not updated"));
     };
 
-    @Api(path = "/api/v1/delete/{id}", method = "GET", produces = "application/json", description = "")
+    @Api(path = "/api/v2/delete/{id}", method = "GET", produces = "application/json", description = "")
     Action deleteToDo = (HttpServletRequest req, HttpServletResponse resp) -> {
         String objId = getPathVariables(req).get("id");
         Observable.just(req)
